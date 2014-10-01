776f4ef69d8e7bfbdaa56da36380ef3dd614f8d8
Daniel Patterson <dp28@st-andrews.ac.uk>
2014-10-01 22:13:53 +0100

    Add secure password to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+
   MAX_USER_NAME_LENGTH = 64
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i # Accepts consecutive dots ...
 
   validates :name, presence: true, length: { maximum: MAX_USER_NAME_LENGTH }
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
             uniqueness: { case_sensitive: false }
-            
-  before_save { self.email = email.downcase }
+  validates :password, length: { minimum: 6 }
+  has_secure_password
 end

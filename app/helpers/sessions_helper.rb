0ae56a98da7263a8b3efdc733a92f68ba88c8d22
Daniel Patterson <dp28@st-andrews.ac.uk>
2014-10-08 23:06:05 +0100

    Add UserController #edit, #update and #index

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,6 @@
 module SessionsHelper
 
-  def sign_in(user)
+  def sign_in user 
     remember_token = User.new_remember_token
     cookies.permanent[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.digest(remember_token))
@@ -17,11 +17,24 @@ module SessionsHelper
     !current_user.nil?
   end
 
-  def current_user=(user)
+  def current_user= user
     @current_user = user
   end
 
   def current_user
     @current_user ||= User.find_by(remember_token: User.digest(cookies[:remember_token]))
+  end
+
+  def current_user? user 
+    user == current_user
+  end
+
+  def redirect_back_or default 
+    redirect_to session[:return_to] || default
+    session.delete :return_to 
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
   end
 end

91decfb77b87a8c3db8cd5d2dd84fec0e7bda9fa
Daniel Patterson <dp28@st-andrews.ac.uk>
2014-11-02 00:12:00 +0000

    Remove mistaken migration

--- db/migrate/20141019191603_remove_activation_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveActivationFromUsers < ActiveRecord::Migration
-  def change
-    [:activation_digest, :activated, :activated_at].each { | column| remove_column :users, column }
-  end
-end
